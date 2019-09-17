6517d6ef2805f7357e12ce7cc30da9b56e2a6feb
eevan7a9 <e7a9@protonmail.com>
2019-09-17 09:17:04 +0900

    added controler middleware

--- voting-backend/app/Http/Controllers/AnswerController.php
+++ voting-backend/app/Http/Controllers/AnswerController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class AnswerController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware("auth:api")->except(["index", "show"]);
+    }
     /**
      * Display a listing of the resource.
      *
