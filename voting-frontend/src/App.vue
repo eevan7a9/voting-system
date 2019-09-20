a6b9f971a055d614730cf66b3cca6ce572d1f83b
eevan7a9 <e7a9@protonmail.com>
2019-09-20 19:35:47 +0900

    added Navbar

--- voting-frontend/src/App.vue
+++ voting-frontend/src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Navbar />
+    <router-view class="container" />
   </div>
 </template>
-
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
 <style>
+@import url("./assets/css/style.css");
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ voting-frontend/src/components/Navbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="nav" class="bg-violetdient">
+    <div class="container tx-upp fw-bold ls-2">
+      <div>
+        <router-link to="/" class="nav-item light padx-1">Home</router-link>
+        <router-link to="/about" class="nav-item light padx-1">About</router-link>
+      </div>
+      <div class="logo">
+        <img class="vue-logo" src="../assets/logo.png" width="35px" />
+        <span>+</span>
+        <img class="laravel-logo" src="../assets/laravel.svg" width="30px" />
+      </div>
+      <div>
+        <router-link to="/" class="nav-item light padx-1">Register</router-link>
+        <router-link to="/about" class="nav-item light padx-1">Login</router-link>
+        <router-link to="/about" class="nav-item light padx-1">Logout</router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+}
+.nav-item {
+  text-decoration: none;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  line-height: 50px;
+}
+.nav-item:hover {
+  background: rgba(194, 176, 213, 0.28);
+}
+.logo {
+  background: #e8dede;
+  border-radius: 100%;
+  padding: 0 10px;
+}
+.logo span {
+  padding-bottom: 20px;
+  position: absolute;
+  font-size: 25px;
+  color: blueviolet;
+}
+.vue-logo {
+  padding: 2px 0;
+  margin-right: 0px;
+}
+.laravel-logo {
+  margin-bottom: -5px;
+  margin-left: 15px;
+}
+</style>
