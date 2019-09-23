7dbdd1b13aba9364cc293dc14d8c5ffc68bec8e4
eevan7a9 <e7a9@protonmail.com>
2019-09-24 08:57:13 +0900

    added loaders

--- voting-frontend/src/App.vue
+++ voting-frontend/src/App.vue
@@ -1,7 +1,24 @@
 <template>
   <div id="app">
-    <Navbar />
-    <router-view class="container main-view" />
+    <Navbar id="topNav" />
+
+    <div id="loader" v-if="loader">
+      <div class="lds-spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+    <router-view class="container main-view" v-if="!loader" />
   </div>
 </template>
 <script>
@@ -9,6 +26,21 @@ import Navbar from "./components/Navbar";
 export default {
   components: {
     Navbar
+  },
+  data() {
+    return {
+      loader: 1
+    };
+  },
+  methods: {
+    loaderTrigger() {
+      this.loader = 0;
+    }
+  },
+  created() {
+    setTimeout(() => {
+      this.loader = 0;
+    }, 3000);
   }
 };
 </script>
@@ -23,6 +55,102 @@ export default {
 @media (max-width: 600px) {
   .main-view {
     padding-top: 60px;
+  }
+}
+#topNav {
+  top: 0;
+  position: sticky;
+  z-index: 1;
+}
+#loader {
+  position: sticky;
+  left: 0;
+  top: 0;
+  z-index: 14;
+  background: rgb(218, 215, 221);
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.lds-spinner {
+  color: official;
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+}
+.lds-spinner div {
+  transform-origin: 32px 32px;
+  animation: lds-spinner 1.2s linear infinite;
+}
+.lds-spinner div:after {
+  content: " ";
+  display: block;
+  position: absolute;
+  top: 3px;
+  left: 29px;
+  width: 5px;
+  height: 14px;
+  border-radius: 20%;
+  background: #1583c7;
+}
+.lds-spinner div:nth-child(1) {
+  transform: rotate(0deg);
+  animation-delay: -1.1s;
+}
+.lds-spinner div:nth-child(2) {
+  transform: rotate(30deg);
+  animation-delay: -1s;
+}
+.lds-spinner div:nth-child(3) {
+  transform: rotate(60deg);
+  animation-delay: -0.9s;
+}
+.lds-spinner div:nth-child(4) {
+  transform: rotate(90deg);
+  animation-delay: -0.8s;
+}
+.lds-spinner div:nth-child(5) {
+  transform: rotate(120deg);
+  animation-delay: -0.7s;
+}
+.lds-spinner div:nth-child(6) {
+  transform: rotate(150deg);
+  animation-delay: -0.6s;
+}
+.lds-spinner div:nth-child(7) {
+  transform: rotate(180deg);
+  animation-delay: -0.5s;
+}
+.lds-spinner div:nth-child(8) {
+  transform: rotate(210deg);
+  animation-delay: -0.4s;
+}
+.lds-spinner div:nth-child(9) {
+  transform: rotate(240deg);
+  animation-delay: -0.3s;
+}
+.lds-spinner div:nth-child(10) {
+  transform: rotate(270deg);
+  animation-delay: -0.2s;
+}
+.lds-spinner div:nth-child(11) {
+  transform: rotate(300deg);
+  animation-delay: -0.1s;
+}
+.lds-spinner div:nth-child(12) {
+  transform: rotate(330deg);
+  animation-delay: 0s;
+}
+@keyframes lds-spinner {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
   }
 }
 </style>
