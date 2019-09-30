038eac46e9d13f4e2561a18e09c38880b45ef9bb
eevan7a9 <e7a9@protonmail.com>
2019-09-30 17:44:44 +0900

    added popup event when finish registration

--- voting-frontend/src/components/Alert.vue
+++ voting-frontend/src/components/Alert.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <div class="alert container dark pady-1 padx-2 mgy-1 borad-2 danger">
+    <div
+      class="alert container dark pady-1 padx-2 mgy-1 borad-2"
+      :class="alert_user.error ? 'danger':'success'"
+      v-show="alert_user.show == 1"
+    >
       <span>
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -13,7 +17,7 @@
           stroke-linecap="round"
           stroke-linejoin="round"
           class="feather feather-check-circle"
-          v-if="!user_message.error"
+          v-if="!alert_user.error"
         >
           <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
           <polyline points="22 4 12 14.01 9 11.01" />
@@ -29,7 +33,7 @@
           stroke-linecap="round"
           stroke-linejoin="round"
           class="feather feather-alert-triangle"
-          v-if="user_message.error"
+          v-if="alert_user.error"
         >
           <path
             d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"
@@ -38,7 +42,7 @@
           <line x1="12" y1="17" x2="12" y2="17" />
         </svg>
       </span>
-      <p class="fs-20">{{ user_message.message }}</p>
+      <p class="fs-20">{{ alert_user.message }}</p>
       <span class="pointer" @click="close">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -61,13 +65,14 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Alert",
-  computed: mapGetters(["user_message"]),
+  computed: mapGetters(["alert_user"]),
   methods: {
+    ...mapActions(["closeUserMessage"]),
     close() {
-      console.log("close alert");
+      this.closeUserMessage();
     }
   }
 };
--- voting-frontend/src/modules/users.js
+++ voting-frontend/src/modules/users.js
@@ -5,15 +5,17 @@ const state = {
     user: {},
     user_message: {
         message: "Some message",
-        error: 1
+        error: 0,
+        show: 1
     },
 };
 const getters = {
     current_user: state => state.user,
     is_login: state => state.login,
-    user_message: state => state.user_message
+    alert_user: state => state.user_message
 };
 const actions = {
+    closeUserMessage: ({ commit }) => commit("clearUserMessage"),
     registerUser: async ({ commit }, user) => {
         const new_user = await axios
             .post("users", {
@@ -31,7 +33,15 @@ const actions = {
     }
 };
 const mutations = {
-    successRegister: (state, user) => (state.user_message = `${user.email} you are now  Registered!!!`)
+    successRegister: (state, user) => {
+        state.user_message.show = 1;
+        state.user_message.message = `${user.email} you are now  Registered!!!`;
+        state.user_message.error = 0;
+    },
+    clearUserMessage: (state) => {
+        state.user_message.message = "";
+        state.user_message.show = 0;
+    },
 };
 export default {
     state,
