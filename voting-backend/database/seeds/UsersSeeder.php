416747435a9f9ac69dc5678a95f9c0aff1f629f0
eevan7a9 <e7a9@protonmail.com>
2019-10-05 22:37:41 +0900

    corrected spelling

--- voting-backend/database/seeds/UsersSeeder.php
+++ voting-backend/database/seeds/UsersSeeder.php
@@ -16,37 +16,37 @@ class UsersSeeder extends Seeder
             "id" => 1,
             "name" => "Sample User_1",
             "email" => "user1@gmail.com",
-            "password" => bcrypt('secret')
+            "password" => bcrypt('secret'),
         ]);
         User::create([
             "id" => 2,
             "name" => "Sample User_2",
             "email" => "user2@gmail.com",
-            "password" => bcrypt('secret')
+            "password" => bcrypt('secret'),
         ]);
         User::create([
             "id" => 3,
             "name" => "Sample User_3",
             "email" => "user3@gmail.com",
-            "password" => bcrypt('secret')
+            "password" => bcrypt('secret'),
         ]);
         User::create([
             "id" => 4,
             "name" => "Sample User_4",
             "email" => "user4@gmail.com",
-            "password" => bcrypt('secret')
+            "password" => bcrypt('secret'),
         ]);
         User::create([
             "id" => 5,
             "name" => "Sample User_5",
             "email" => "user5@gmail.com",
-            "password" => bcrypt('secret')
+            "password" => bcrypt('secret'),
         ]);
         User::create([
             "id" => 6,
             "name" => "Sample User_6",
             "email" => "user6@gmail.com",
-            "password" => bcrypt('secret')
+            "password" => bcrypt('secret'),
         ]);
     }
 }
