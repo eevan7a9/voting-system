aad0fa2bd83bbf840283d3535dff87ad2e1de4ac
eevan7a9 <e7a9@protonmail.com>
2019-10-07 20:04:52 +0900

    added additional_info on questions table

--- voting-backend/database/migrations/2019_10_07_105756_add_additional_info_to_questions.php
+++ voting-backend/database/migrations/2019_10_07_105756_add_additional_info_to_questions.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDescriptionToQuestions extends Migration
+class AddAdditionalInfoToQuestions extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class AddDescriptionToQuestions extends Migration
     public function up()
     {
         Schema::table('questions', function (Blueprint $table) {
-            $table->text("description")->after("title")->nullable();
+            $table->text("additional_info")->after('description')->nullable();
+
         });
     }
 
@@ -26,7 +27,8 @@ class AddDescriptionToQuestions extends Migration
     public function down()
     {
         Schema::table('questions', function (Blueprint $table) {
-            $table->dropColumn("description");
+            $table->dropColumn("additional_info");
+
         });
     }
 }
