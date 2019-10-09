b277cf236d5eb7e9160357a2f1b37063cfdbfdd3
eevan7a9 <e7a9@protonmail.com>
2019-10-09 10:29:08 +0900

    added questions on votes when calling user/account

--- voting-backend/app/User.php
+++ voting-backend/app/User.php
@@ -2,14 +2,13 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use  HasApiTokens, Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -44,6 +43,6 @@ class User extends Authenticatable
     }
     public function votes()
     {
-        return $this->hasMany(Vote::class, 'user_id');
+        return $this->hasMany(Vote::class, 'user_id')->with('question');
     }
 }
