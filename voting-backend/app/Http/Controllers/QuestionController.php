8d3f4a879f0c3344a98e4da4d37192aac3c9894c
eevan7a9 <e7a9@protonmail.com>
2019-10-10 19:31:55 +0900

    added case for 'all'

--- voting-backend/app/Http/Controllers/QuestionController.php
+++ voting-backend/app/Http/Controllers/QuestionController.php
@@ -124,15 +124,21 @@ class QuestionController extends Controller
         $order = $sort == "newest" ? "desc" : "asc"; // check if not newest then sort to oldest
 
         switch ($filter) {
+            case 'all':
+                $questions = Question::orderBy('created_at', $order)->get(); // order by newest
+                foreach ($questions as $question) {
+                    $question->answers->makeHidden('votes'); // we want to show the hasMany retationship of question
+                }
+                break;
             case 'not-voted': // we get if user did not vote on the question
-                $questions = Question::whereDoesntHave('votes', function ($vote) {
+                $questions = Question::with('answers')->whereDoesntHave('votes', function ($vote) {
                     $user_id = Auth::user()->id;
                     $vote->where('user_id', $user_id);
                 })->orderBy('created_at', $order)->get();
 
                 break;
             default: // we get if user voted on the question
-                $questions = Question::whereHas('votes', function ($vote) {
+                $questions = Question::with('answers')->whereHas('votes', function ($vote) {
                     $user_id = Auth::user()->id;
                     $vote->where('user_id', $user_id);
                 })->orderBy('created_at', $order)->get();
