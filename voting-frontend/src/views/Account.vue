3fd603d1a4f0b05a8104106da47b84e2166fa205
eevan7a9 <e7a9@protonmail.com>
2020-03-05 20:54:16 +0900

    add footer

--- voting-frontend/src/views/Account.vue
+++ voting-frontend/src/views/Account.vue
@@ -1,52 +1,63 @@
 <template>
-  <div class="wrapper mgt-1">
-    <div class="creadentials borad-2">
-      <div class="inner-creadentials pady-3 padx-1">
-        <label class="pady-1 fs-normal fw-bold ls-1 mgt-1 tx-upp">Username :</label>
-        <p class="pady-1 fs-18 ls-1 mgt-1">{{ current_user.name }}</p>
-        <label class="pady-1 fs-normal fw-bold ls-1 mgt-1 tx-upp">Email :</label>
-        <p class="pady-1 fs-18 ls-1 mgt-1">{{ current_user.email }}</p>
-        <label class="pady-1 fs-normal fw-bold ls-1 mgt-1 tx-upp">Joined :</label>
-        <p class="pady-1 fs-18 ls-1 mgt-1">{{ current_user.created_at }}</p>
-      </div>
-      <div class="settings">
-        <div class="inner-setting">
-          <label class="padx-1">Edit Password</label>
-          <router-link :to="{name:'updatepassword'}">
-            <button class="pady-1 fs-18 light borad-1 padx-1 bg-bluedient pointer">Update Password</button>
-          </router-link>
+  <div>
+    <div class="wrapper container mgt-1">
+      <div class="creadentials borad-2">
+        <div class="inner-creadentials pady-3 padx-1">
+          <label class="pady-1 fs-normal fw-bold ls-1 mgt-1 tx-upp">Username :</label>
+          <p class="pady-1 fs-18 ls-1 mgt-1">{{ current_user.name }}</p>
+          <label class="pady-1 fs-normal fw-bold ls-1 mgt-1 tx-upp">Email :</label>
+          <p class="pady-1 fs-18 ls-1 mgt-1">{{ current_user.email }}</p>
+          <label class="pady-1 fs-normal fw-bold ls-1 mgt-1 tx-upp">Joined :</label>
+          <p class="pady-1 fs-18 ls-1 mgt-1">{{ current_user.created_at }}</p>
         </div>
-      </div>
-    </div>
-    <div class="activity mgt-1">
-      <div class="voted-questions pady-2 padx-1">
-        <h2 class="pady-1 tx-upp">Recently voted surveys</h2>
-        <div class="inner-voted pady-1 padx-5-px">
-          <div class="mgt-1 pady1" v-for="(question, index) in current_user.votes" :key="index">
-            <router-link :to="{ name:'details', params:{questionId:question.question.id}}">
-              <p class="pointer ls-1">{{ question.question.title }}</p>
+        <div class="settings">
+          <div class="inner-setting">
+            <label class="padx-1">Edit Password</label>
+            <router-link :to="{name:'updatepassword'}">
+              <button class="pady-1 fs-18 light borad-1 padx-1 bg-bluedient pointer">Update Password</button>
             </router-link>
           </div>
         </div>
       </div>
-      <div class="asked-questions pady-2 padx-1">
-        <h2 class="pady-1 tx-upp">Recently asked surveys</h2>
-        <div class="inner-asked pady-1 padx-5-px">
-          <div class="mgt-1 pady1" v-for="(question, index) in current_user.questions" :key="index">
-            <router-link :to="{ name:'details', params:{questionId:question.id}}">
-              <p class="pointer ls-1">{{ question.title }}</p>
-            </router-link>
+      <div class="activity mgt-1">
+        <div class="voted-questions pady-2 padx-1">
+          <h2 class="pady-1 tx-upp">Recently voted surveys</h2>
+          <div class="inner-voted pady-1 padx-5-px">
+            <div class="mgt-1 pady1" v-for="(question, index) in current_user.votes" :key="index">
+              <router-link :to="{ name:'details', params:{questionId:question.question.id}}">
+                <p class="pointer ls-1">{{ question.question.title }}</p>
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="asked-questions pady-2 padx-1">
+          <h2 class="pady-1 tx-upp">Recently asked surveys</h2>
+          <div class="inner-asked pady-1 padx-5-px">
+            <div
+              class="mgt-1 pady1"
+              v-for="(question, index) in current_user.questions"
+              :key="index"
+            >
+              <router-link :to="{ name:'details', params:{questionId:question.id}}">
+                <p class="pointer ls-1">{{ question.title }}</p>
+              </router-link>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from "@/components/Footer";
 import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Account",
+  components: {
+    Footer
+  },
   computed: {
     ...mapGetters(["current_user"])
   },
@@ -71,6 +82,9 @@ export default {
 a {
   text-decoration: none;
   color: #333;
+}
+.wrapper {
+  height: 100vh;
 }
 .settings {
   display: flex;
